19d04a98e0b6b43ace1aee72399ff8d4cc5b1b55
rizo0103rizo.sho <muhammadrizo.shokirov2006@gmail.com>
2025-09-28 01:30:32 +0500

    added route get-user-data

--- routes.js
+++ routes.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const connection = require('./db');
+const jwt = require("jsonwebtoken");
+const { private } = require("./configs");
+
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -15,16 +18,57 @@ router.post("/login", (req, res) => {
         const filter = [username, password];
 
         connection.query(sql, filter, (err, results) => {
-            if (err) console.log(err);
-            console.log(results);
+            if (err) return res.status(500).json({ message: "error occured" + err });
 
-            return res.status(200).json({ message: "success", data: results });
-        });    
+            if (results.length) {
+                const token = jwt.sign({ username, password }, private);
+                return res.status(200).json({ message: "success", data: token });
+            } else {
+                return res.status(404).json({ message: "user was not found..." });
+            }
+
+        });
         
     } catch (error) {
         return res.status(500).json({ message: "server error" + error });
-    }    
+    }
+});
 
+router.get("/get-user-data", (req, res) => {
+    const { token } = req.headers;
+
+    try {
+        let username = "", password = "";
+
+        jwt.verify(token, private, (err, decoded) => {
+            if (err) {
+                return res.status(400).json({ message: err });
+            }
+            
+            username = decoded.username;
+            password = decoded.password;
+        });
+
+        if (!username || !password) {
+            return res.status(400).json({ message: "invalid token" });
+        }
+
+        const sql = `SELECT * FROM users WHERE username = ? AND password = ?`;
+        const filter = [username, password];
+        
+        connection.query(sql, filter, (err, results) => {
+            if (err) return res.status(500).json({ message: "DB error" + err });
+
+            if (results.length) {
+                return res.status(200).json({ message: "success", data: results });
+            } else {
+                return res.status(404).json({ message: "user data was not found" });
+            }
+        });
+
+    } catch (error) {
+        return res.status(500).json({ message: "server error" + error });
+    }
 });
 
 router.post("/register", async (req, res) => {
