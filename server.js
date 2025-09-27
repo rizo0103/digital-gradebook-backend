085b2ce2d986d80e6d22b2aa2fc91b5ec33225de
rizo0103rizo.sho <muhammadrizo.shokirov2006@gmail.com>
2025-09-28 00:15:30 +0500

    created mini server with base routes and installed mysql modules

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    return res.send("Hello, World!");
+});
+
+app.get("/hello/:name", (req, res) => {
+    return res.send(`Hello, ${req.params.name}`);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
