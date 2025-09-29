cf503c66223355d2f9cdd8f0c7d3faad2278d248
rizo0103rizo.sho <muhammadrizo.shokirov2006@gmail.com>
2025-09-29 13:33:14 +0500

    added cors policy

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = require('./routes');
+const cors = require('cors');
 const { ip } = require('./configs.js');
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-
+app.use(cors());
 app.use(router);
 
 app.listen(PORT, ip, () => {
