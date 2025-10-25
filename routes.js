c90029b014a6e6b5f5bd7be7bc041f13cbb4a5e0
rizo0103rizo.sho <muhammadrizo.shokirov2006@gmail.com>
2025-10-25 17:51:21 +0500

    wrote request for getting group data by name

--- routes.js
+++ routes.js
@@ -120,4 +120,24 @@ router.post("/register", async (req, res) => {
 
 });
 
+router.post("/get-group-data", async(req, res) => {
+    const { name } = await req.body;
+    
+    try {
+        const sql = "SELECT * FROM groups WHERE name = ? VALUE ?";
+
+        connection.query(sql, [name], (err, results) => {
+            if (err) console.log(err);
+            
+            console.log(logs(req).ok);
+            
+            return res.status(200).json({message: "success", data: results});
+        });
+    } catch (err) {
+        console.error(logs(req).err);        
+        
+        return res.status(500).json({message: "server error " + err});
+    }
+});
+
 module.exports = router;
