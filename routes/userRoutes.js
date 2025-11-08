3e11abf1c1129e0c368c68e0af35740332345892
rizo0103rizo.sho <muhammadrizo.shokirov2006@gmail.com>
2025-11-08 15:58:45 +0500

    wrote route for updating user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -114,7 +114,7 @@ userRouter.get("/get-user-data/:username", async (req, res) => {
             console.error(logs(req).err);
             return res.status(401).json({ message: "unauthorized" });
         }
-        
+
         // verify admin status
         if (requestingUser.status !== "admin") {
             console.error(logs(req).err);
@@ -135,6 +135,44 @@ userRouter.get("/get-user-data/:username", async (req, res) => {
     } catch (error) {
         console.error(logs(req).err);
         
+        return res.status(500).json({ message: "server error " + error });
+    }
+});
+
+userRouter.put("/update-user/:username", async (req, res) => {
+    const { username } = req.params;
+    const { new_username, new_password, new_fullname_korean, new_fullname_english, new_groups, new_status } = req.body;
+    const { token } = req.headers;
+
+    try {
+        // verify token
+        const requestingUser = await getUserFromToken(token, jwt, private, connection);
+        
+        if (!requestingUser) {
+            console.error(logs(req).err);
+            return res.status(401).json({ message: "unauthorized" });
+        }
+        // verify admin status
+
+        if (requestingUser.status !== "admin") {
+            console.error(logs(req).err);
+            return res.status(403).json({ message: "forbidden" });
+        }
+
+        let first_name_english = new_fullname_english.split(" ")[0],
+            last_name_english = new_fullname_english.split(" ")[1],
+            first_name_korean = new_fullname_korean.split(" ")[0],
+            last_name_korean = new_fullname_korean.split(" ")[1];
+        
+        const sql = "UPDATE users SET english_first_name = ?, english_last_name = ?, korean_first_name = ?, korean_last_name = ?, username = ?, password = ?, groups = ?, status = ? WHERE username = ?";
+        const [result] = await connection.promise().query(sql, [first_name_english, last_name_english, first_name_korean, last_name_korean, new_username, new_password, JSON.stringify(new_groups), new_status, username]);
+
+        console.log(logs(req).ok);
+
+        return res.status(200).json({ message: "user updated ", data: result });
+    } catch (error) {
+        console.error(logs(req).err);
+
         return res.status(500).json({ message: "server error " + error });
     }
 });
