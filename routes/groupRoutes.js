f8ee7767e016a7d60dcc6b2c921299acf32baba0
rizo0103rizo.sho <muhammadrizo.shokirov2006@gmail.com>
2025-11-10 11:07:32 +0500

    wrote update-group by id route

--- routes/groupRoutes.js
+++ routes/groupRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const connection = require('../db');
 const jwt = require("jsonwebtoken");
 const { private } = require("../configs");
-const { logs, getUserFromToken } = require('../utils/common');
+const { logs, getUserFromToken, modifyDays } = require('../utils/common');
 
 const groupRouter = express.Router();
 
@@ -45,36 +45,8 @@ groupRouter.post("/create-group", async(req, res) => {
     if (!token) {
         console.error(logs(req).err);
     }
-
     
-    const modifiedDays = days.map(day => {
-        let year = `${day}`.substring(0, 4),
-            month = `${day}`.substring(4, 6),
-            date = `${day}`.substring(6, 8);
-
-        return `${year}-${month}-${date}`;
-    }), months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-    const finalDays = {};
-        
-    modifiedDays.map(day => {
-        let dateObj = new Date(day);
-        let year = dateObj.getFullYear();
-        let month = months[dateObj.getMonth()];
-
-        finalDays[year] = finalDays[year] || {};
-        finalDays[year][month] = finalDays[month] || [];
-    });
-
-        
-    modifiedDays.map(day => {
-        let dateObj = new Date(day);
-        let year = dateObj.getFullYear();
-        let month = months[dateObj.getMonth()];
-        let date = dateObj.getDate();
-        
-        finalDays[year][month].push(date);
-    });
+    let finalDays = modifyDays(days);
 
     try {
         // verify token (throws if invalid)
@@ -91,7 +63,7 @@ groupRouter.post("/create-group", async(req, res) => {
         }
 
         // create group
-        const sql = "INSERT INTO groups (name, amount, days) VALUES ( ?, ?, ?)";
+        const sql = "INSERT INTO groups (name, amount, days) VALUES (?, ?, ?)";
         const [result] = await connection.promise().query(sql, [name, amount, JSON.stringify(finalDays)]);
 
         console.log(logs(req).ok);
@@ -193,6 +165,55 @@ groupRouter.get("/get-group-data/:id", async (req, res) => {
 
         let sql = "SELECT * FROM groups WHERE id = ?",
             [results] = await connection.promise().query(sql, id);
+        
+        if (results.length === 0) {
+            console.error(logs(req).err);
+
+            return res.status(404).json({ message: "group was not found" });
+        }
+
+        console.log(logs(req).ok);
+
+        return res.status(200).json({ message: "success", data: results[0] });
+    } catch (error) {
+        console.error(logs(req).err);
+
+        return res.status(500).json({ message: "server error" + error });
+    }
+});
+
+groupRouter.put("/update-group/:id", async (req, res) => {
+    const { id } = req.params;
+    const { new_name, new_amount, new_days } = req.body;
+    const { token } = req.headers;
+
+    if (new_days.length === 0) {
+        console.error(logs(req).err);
+
+        return res.status(400).json({ message: "no days provided" });
+    }
+
+    let finalDays = modifyDays(new_days);
+
+    try {
+        // verify token (throws if invalid)
+        const user = await getUserFromToken(token, jwt, private, connection);
+
+        if (!user) {
+            console.error(logs(req).err);
+
+            return res.status(401).json({ message: "unauthorized" });
+        }
+
+        // verify admin status
+        if (user.status !== "admin") {
+            console.error(logs(req).err);
+
+            return res.status(403).json({ message: "forbidden" });
+        }
+
+        let sql = "UPDATE groups SET name = ?, amount = ?, days = ? WHERE id = ?",
+            [results] = await connection.promise().query(sql, [new_name, new_amount, JSON.stringify(finalDays), id]);
         
         if (results.length === 0) {
             console.error(logs(req).err);
