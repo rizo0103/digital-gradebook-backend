468d0f236201d55a8ed0302bfb6ef506c88c6e4d
rizo0103rizo.sho <muhammadrizo.shokirov2006@gmail.com>
2026-01-06 17:10:25 +0500

    added get-group-data/:id for mobile

--- routes/mobile/groupRoutes.js
+++ routes/mobile/groupRoutes.js
@@ -26,4 +26,65 @@ mobileGroupRouter.get("/mobile/get-groups", async (req, res) => {
     }
 });
 
+mobileGroupRouter.get("/mobile/get-group-data/:id", async (req, res) => {
+    const { id } = req.params;
+    
+    try {
+        
+        const pool = await connectToCloudSQL;
+        const [attendanceTable] = await pool.execute("SHOW TABLES LIKE 'attendance'");
+
+        const group_students_sql = `SELECT * FROM group_students WHERE group_id = ?`,
+            group_data_sql = `SELECT * FROM \`groups\` WHERE id = ?`,
+            group_schedule_sql = `SELECT * FROM timeslots WHERE name = ?`,
+            group_attendance_sql = `SELECT * FROM attendance WHERE group_id = ?`;
+
+        let [group_students] = await pool.execute(group_students_sql, [id]),
+            [group_data] = await pool.execute(group_data_sql, [id]),
+            [group_schedule] = await pool.execute(group_schedule_sql, [group_data[0].schedule]),
+            group_attendance;
+
+        if (attendanceTable.length !== 0) {
+            [group_attendance] = await pool.execute(group_attendance_sql, [id]);
+        } else {
+            group_attendance = [];
+        }
+
+        if (group_data.length === 0) {
+            console.error(logs(req).err, " no group data found");
+            return res.status(404).json({ message: "no data found" });
+        }
+
+        if (group_students.length === 0) {
+            console.error(logs(req).err, " no group students data found");
+            return res.status(404).json({ message: "no data found" });
+        }
+
+        if (group_schedule.length === 0) {
+            console.error(logs(req).err, " no data found");
+            return res.status(404).json({ message: "no group schedule data found" });
+        }
+
+        if (group_attendance.length === 0) {
+            console.error(logs(req).err, " no group attendance data found");
+            group_attendance = [];
+        }
+
+        console.log(logs(req).ok, " successfuly get data from `group_students`");
+
+        return res.status(200).json({ message: "success", data: { 
+            group_students,
+            group_data: group_data[0],
+            group_schedule: {
+                ...group_schedule[0],
+                days: modifyDays(JSON.parse(group_schedule[0].timeslot))
+            },
+            group_attendance
+        }});
+    } catch (error) {
+        console.error(logs(req).err, error);
+        return res.status(500).json({ message: "server error" });
+    }
+});
+
 module.exports = mobileGroupRouter;
